8676cbccc7d56e4e35f68e05d8e74285475dad9d
Koleff <martin_koleff@abv.bg>
2024-12-07 14:26:25 +0200

    Updated contact page.

--- src/views/ContactPage.vue
+++ src/views/ContactPage.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
       <h1>Contact Us</h1>
-      <p>Feel free to reach out at example@example.com for any inquiries!</p>
+      <p>Feel free to reach out at martinkolefff@gmail.com for any inquiries!</p>
     </div>
   </template>
   
